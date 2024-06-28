f4591f06851119337c83f48c5bf787f4196a98a6
Parmanand Sahadeo <parmsdev@protonmail.com>
2024-06-28 07:08:28 -0400

    Add styles to contact page

--- src/routes/Contact.jsx
+++ src/routes/Contact.jsx
@@ -4,8 +4,43 @@ import './Contact.css';
 function Contact() {
     return (
         <>
-            <div class="row">
-               <img class="coming-soon" src={comingSoon} alt="Coming soon image" /> 
+            <div class="row contact-bg">
+
+            <div class="row mx-5">
+                <div class="col-6 offset-3 my-2">
+                    <h2 class="my-3">Contact</h2>
+                    <p>
+                        Don't hesitate to reach out for any questions or concerns!
+                    </p>
+                </div>
+                
+                <div class="col-6 offset-3">
+                    <div class="row">
+                        <form class="mb-5">
+                            <div class="form-group row mb-3">
+                                <label for="name" class="col-sm-2 col-form-label">Name</label>
+                                <div class="col-sm-10">
+                                    <input type="text" class="form-control" id="name"/>
+                                </div>
+                            </div>
+                            <div class="form-group row mb-3">
+                                <label for="email" class="col-sm-2 col-form-label">Email</label>
+                                <div class="col-sm-10">
+                                    <input type="email"  class="form-control" id="email" placeholder="email@example.com" />
+                                </div>
+                            </div>
+                            <div class="form-group row mb-3">
+                                <label for="message" class="col-sm-2 col-form-label">Message</label>
+                                <div class="col-sm-10">
+                                    <textarea class="form-control" id="message" rows="3"></textarea>
+                                </div>
+                            </div>
+                            <button type="submit" class="btn btn-dark float-end">Submit</button>
+                        </form>
+
+                    </div>
+                </div>
+            </div>
             </div>
         </>
     )
--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,10 +1,4 @@
-import { useState } from 'react'
-import reactLogo from '../assets/react.svg'
-import viteLogo from '/vite.svg'
-
 import HeroBanner from '../components/HeroBanner';
-import NavMenu from '../components/NavMenu';
-import Footer from '../components/Footer';
 import Section from '../components/Section';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
