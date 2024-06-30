133b177649d7f3f5e48e14c8010a41d12fc04585
Parmanand Sahadeo <parmsdev@protonmail.com>
2024-06-29 23:01:47 -0400

    Edits to contact page styles

--- src/routes/Contact.jsx
+++ src/routes/Contact.jsx
@@ -1,42 +1,37 @@
 import './Contact.css';
-
 function Contact() {
     return (
         <>
-            <div class="row contact-bg p-5">
-                <div class="col-12 col-md-8 offset-md-2">
-                    <div class="col-12 p-3">
-                        <h2 class="my-3">Contact</h2>
-                        <p>
-                            Don't hesitate to reach out for any questions or concerns!
-                        </p>
+            <div class="row py-4">
+                <div class="container-1200 m-auto">
+                    <div class="col-12">
+                        <h2 class="my-2">Contact</h2>
                     </div>
+                </div>
+            </div>
                     
-                    <div class="col-12 p-3">
-                        <div class="row">
-                            <form class="my-2 my-lg-3">
-                                <div class="form-group row mb-4">
-                                    <label for="name" class="col-sm-2 col-form-label">Name</label>
-                                    <div class="col-sm-10">
-                                        <input type="text" class="form-control" id="name"/>
-                                    </div>
-                                </div>
-                                <div class="form-group row mb-4">
-                                    <label for="email" class="col-sm-2 col-form-label">Email</label>
-                                    <div class="col-sm-10">
-                                        <input type="email"  class="form-control" id="email" placeholder="email@example.com" />
-                                    </div>
-                                </div>
-                                <div class="form-group row mb-4">
-                                    <label for="message" class="col-sm-2 col-form-label">Message</label>
-                                    <div class="col-sm-10">
-                                        <textarea class="form-control" id="message" rows="3"></textarea>
-                                    </div>
-                                </div>
-                                <button type="submit" class="btn btn-dark float-end">Submit</button>
-                            </form>
+            <div class="row contact-bg p-5">
+                <div class="rounded col-12 col-md-10 col-lg-6 offset-md-1 offset-lg-3 py-3">
+                    <p class="fw-bold">Don't hesitate to reach out for any questions or concerns!</p>
+                    <form>
+                        <div class="form-group mb-4">
+                            <label for="name" class="form-label fw-bold">Name</label>
+                            <input type="text" class="form-control" id="name"/>
                         </div>
-                    </div>
+                        <div class="form-group mb-4">
+                            <label for="email" class="form-label fw-bold">Email</label>
+                            <input type="email"  class="form-control" id="email" placeholder="email@example.com" />
+                        </div>
+                        <div class="form-group mb-4">
+                            <label for="phone-number" class="form-label fw-bold">Phone Number</label>
+                            <input type="phone-number"  class="form-control" id="phone-number" />
+                        </div>
+                        <div class="form-group mb-4">
+                            <label for="message" class="form-label fw-bold">Message</label>
+                            <textarea class="form-control" id="message" rows="3"></textarea>
+                        </div>
+                        <button type="submit" class="btn btn-dark float-end">Submit</button>
+                    </form>
                 </div>
             </div>
         </>
