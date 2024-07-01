5d29f952403517fa92a0017865923de804f2e2ff
Parmanand Sahadeo <parmsdev@protonmail.com>
2024-07-01 12:04:17 -0400

    Adds new nav logo

--- src/components/NavMenu.jsx
+++ src/components/NavMenu.jsx
@@ -1,6 +1,7 @@
 import './NavMenu.css';
 import NavItem from "./NavItem";
 import NavImg from '../assets/navbar-logo-2.svg';
+import NavLogo from '../assets/maxplogo.png';
 
 function NavMenu() {
 	const items = [
@@ -12,15 +13,15 @@ function NavMenu() {
 		<>
 			<div class="row nav-bg">
 				<div class="container-1200 m-auto">
-					<nav class="navbar navbar-expand-lg py-3">
+					<nav class="navbar navbar-expand-lg">
 						<div class="container-fluid">
 							<a class="navbar-brand text-color-yellow nav-logo" href="/">
-								<img src={NavImg} alt="driving school logo" />
+								<img src={NavLogo} alt="driving school logo" />
 							</a>
 							<button class="navbar-toggler nav-yellow" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 								<span class="navbar-toggler-icon"></span>
 							</button>
-							
+
 							<div class="collapse navbar-collapse" id="navbarSupportedContent">
 							<ul class="navbar-nav me-auto mb-2 mb-lg-0 w-100 d-flex justify-content-end">
 								{items.map( (i)=> <NavItem name={i.name} url={i.url}/> )}
