5e509c844616bd3763cc494739cffc3d4f69b650
Parm Sahadeo <parmsdev@protonmail.com>
2024-12-26 10:52:40 -0500

    26 updated hero banner image (#27)
    * Updates hero banner
    
    * Updates hero banner and text
    
    * Fix spacing issues
    
    * Change background color for video
    
    * Apply bg only to video element

--- src/components/HeroBanner.jsx
+++ src/components/HeroBanner.jsx
@@ -1,29 +1,17 @@
 import './HeroBanner.css';
-// import classes from './HeroBanner.module.css';
-// import HeroImg from '../assets/hero.jpg';
-// import HeroImg from '../assets/hero-desktop.png';
-// import HeroImgMo from '../assets/hero-mobile.png';
-import HeroImg from '../assets/hero-desktop-v1.png';
-import HeroImgMo from '../assets/hero-mobile-v1.png';
 
 function HeroBanner() {
 
-    return (
-        <>
-			<div className="row py-3 py-sm-5 hero-bg">
-                <div className="container-1200 m-auto">
-                    <div className="row">
-                        <picture className="img-fluid hero-img">
-                            <source media="(min-width: 968px)" srcSet={HeroImg}></source>
-                            <source media="(min-width: 767px)" srcSet={HeroImgMo}></source>
-
-                            <img id="hero-img" className="img-fluid w-100 px-0 hero-img" src={HeroImg} alt="hero image"></img>
-                        </picture>
-                    </div>
-                </div>
-            </div>
-        </>
-    )
+	return (
+		<>
+			<div className="row hero-bg hero">
+				<div className="container-1200 m-auto hero-text">
+					<p className="pt-serif-bold">Drive with Confidence</p>
+					<p className="pt-serif-bold">Drive with Max Precision</p>
+				</div>
+			</div>
+		</>
+	)
 }
 
 export default HeroBanner;
