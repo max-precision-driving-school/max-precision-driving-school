4c19bae87200affac81438cd2534ae75a2fa0d49
Parmanand Sahadeo <parmsdev@protonmail.com>
2025-08-28 09:06:46 -0400

    Adds new logo

--- src/components/NavMenu.jsx
+++ src/components/NavMenu.jsx
@@ -1,7 +1,8 @@
 import './NavMenu.css';
 import NavItem from "./NavItem";
 // import NavLogo from '../assets/maxplogo-v2-1.png';
-import NavLogo from '../assets/maxplogo-v2.png';
+// import NavLogo from '../assets/maxplogo-v2.png';
+import NavLogo from '../assets/mp-logo.png';
 
 function NavMenu() {
 	const items = [
